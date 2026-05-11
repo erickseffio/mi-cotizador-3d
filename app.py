b637c287de9e5f8109e30fc5820f75643a935e25
erickseffio <41970730+erickseffio@users.noreply.github.com>
2026-05-11 18:00:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
 # Configuración de la página
-st.set_page_config(page_title="Cotizador 3D - Oferta Lanzamiento", page_icon="🎨" , initial_sidebar_state="collapsed" # Esto la mantiene cerrada al inicio)
+st.set_page_config(
+    page_title="Cotizador 3D - Oferta Lanzamiento", 
+    page_icon="🎨", 
+    initial_sidebar_state="collapsed" # Esto la mantiene cerrada al inicio
+)
 
 st.title("🚀 Cotizador 3D: ¡Promoción de Apertura!")
 st.info("🎁 Calidad premium a precio de taller. ¡Aprovecha nuestros descuentos de inauguración!")
