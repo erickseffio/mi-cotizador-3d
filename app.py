5ffee99eb14868ac2c5392fb54294b9a153460c7
erickseffio <41970730+erickseffio@users.noreply.github.com>
2026-05-12 16:22:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -454,7 +454,7 @@ st.markdown("""
     }
     .card-img {
         width: 100%;
-        height: 140px; 
+        height: 120px; 
         object-fit: cover;
         object-position: center;
     }
